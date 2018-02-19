8351ff37affc9841eb9d1793db6373864279612d
Jevan Pipitone <jevanpipitone@users.noreply.github.com>
2018-02-19 15:36:08 +1100

    update

---
+++ dotnetrdf/vs/Dotnetrdf Test/Dotnetrdf Test/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Dotnetrdf_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(312, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Run using relatives.ttl";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Location = new System.Drawing.Point(39, 121);
+            this.tbOutput.Multiline = true;
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbOutput.Size = new System.Drawing.Size(711, 281);
+            this.tbOutput.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(408, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(342, 47);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Run using Fuseki endpoint";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 418);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ dotnetrdf/vs/Dotnetrdf Test/Dotnetrdf Test/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using VDS.RDF;
+using VDS.RDF.Parsing;
+using VDS.RDF.Query;
+using VDS.RDF.Query.Datasets;
+using VDS.RDF.Writing.Formatting;
+
+namespace Dotnetrdf_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tbOutput.Text = "";
+            tbOutput.Text += "Starting - " + button1.Text + Environment.NewLine;
+            try
+            {
+                // https://github.com/dotnetrdf/dotnetrdf/wiki/UserGuide
+                // https://github.com/dotnetrdf/dotnetrdf/wiki/UserGuide-Reading-RDF
+                IGraph g1 = new Graph();
+                TurtleParser ttlparser = new TurtleParser();
+                ttlparser.Load(g1, "data/relatives.ttl");
+                tbOutput.Text += "Loaded relatives.ttl" + Environment.NewLine;
+
+                // https://github.com/dotnetrdf/dotnetrdf/wiki/UserGuide-Querying-With-SPARQL
+                TripleStore store = new TripleStore();
+                store.Add(g1);
+                InMemoryDataset ds = new InMemoryDataset(store,true);
+                ISparqlQueryProcessor processor = new LeviathanQueryProcessor(ds);
+                SparqlQueryParser parser = new SparqlQueryParser();
+                //SparqlQuery q = parser.ParseFromString("PREFIX : <http://example.org/relatives#> SELECT * WHERE { ?s a ?type }");
+                SparqlQuery q1 = parser.ParseFromString("PREFIX : <http://example.org/relatives#> SELECT ?gc ?gp WHERE { ?gc :hasGrandparent ?gp . ?gc a :Person . }");
+                var results1 = processor.ProcessQuery(q1);
+                if (results1 is SparqlResultSet)
+                {
+                    SparqlResultSet rset1 = (SparqlResultSet)results1;
+                    tbOutput.Text += "Grandparent query gave " + rset1.Count + " result(s)" + Environment.NewLine;
+                }
+                SparqlQuery q2 = parser.ParseFromString("PREFIX : <http://example.org/relatives#> SELECT * WHERE { ?a ?b ?c }");
+                var results2 = processor.ProcessQuery(q2);
+                tbOutput.Text += "Results for * query of all triples:" + Environment.NewLine;
+                /*
+                
+                */
+                // https://github.com/dotnetrdf/dotnetrdf/wiki/UserGuide-Working-With-Triple-Stores
+                if (results2 is SparqlResultSet)
+                {
+                    //Print out the Results
+                    SparqlResultSet rset2 = (SparqlResultSet)results2;
+                    foreach (SparqlResult result in rset2)
+                    {
+                        tbOutput.Text += result.ToString()+Environment.NewLine;
+                    }
+                }
+            } catch (RdfParseException parseEx) {
+                //This indicates a parser error e.g unexpected character, premature end of input, invalid syntax etc.
+                tbOutput.Text += "Parser Error" + Environment.NewLine;
+                tbOutput.Text += parseEx.Message + Environment.NewLine;
+            } catch (RdfException rdfEx) {
+                //This represents a RDF error e.g. illegal triple for the given syntax, undefined namespace
+                tbOutput.Text += "RDF Error" + Environment.NewLine;
+                tbOutput.Text += rdfEx.Message + Environment.NewLine;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tbOutput.Text = "";
+            tbOutput.Text += "Starting - " + button2.Text + Environment.NewLine;
+            try
+            {
+                SparqlRemoteEndpoint endpoint = new SparqlRemoteEndpoint(new Uri("http://127.0.0.1:3030/relativesWithOwlInferencingAndOpenlletReasoner/query"));
+                // must be a CONSTRUCT or DESCRIBE query
+                // https://github.com/dotnetrdf/dotnetrdf/wiki/UserGuide-Working-With-Graphs
+                String query = "PREFIX : <http://example.org/relatives#> CONSTRUCT { ?gc :hasGrandparent ?gp } WHERE { ?gc :hasGrandparent ?gp . ?gc a :Person . }";
+                IGraph g1 = endpoint.QueryWithResultGraph(query);
+                //Print out the Results
+                NTriplesFormatter formatter = new NTriplesFormatter();
+                tbOutput.Text += "Total tuples: " + g1.Triples.Count + Environment.NewLine;
+                foreach (Triple t in g1.Triples)
+                {
+                    tbOutput.Text += t.ToString(formatter) + Environment.NewLine + Environment.NewLine;
+                }
+            } catch (RdfParseException parseEx) {
+                //This indicates a parser error e.g unexpected character, premature end of input, invalid syntax etc.
+                tbOutput.Text += "Parser Error" + Environment.NewLine;
+                tbOutput.Text += parseEx.Message + Environment.NewLine;
+            } catch (RdfException rdfEx) {
+                //This represents a RDF error e.g. illegal triple for the given syntax, undefined namespace
+                tbOutput.Text += "RDF Error" + Environment.NewLine;
+                tbOutput.Text += rdfEx.Message + Environment.NewLine;
+            }
+        }
+    }
+}
