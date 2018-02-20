936b3e036da8a132b4173e03f36d2fea3f17284d
Jevan Pipitone <jevanpipitone@users.noreply.github.com>
2018-02-21 01:43:09 +1100

    update

--- EasyRDF/EasyRDF Projects/src/EasyRDF-test-2.php
+++ EasyRDF/EasyRDF Projects/src/EasyRDF-test-2.php
@@ -2,7 +2,7 @@
 require 'vendor/autoload.php';
 
 $graph = new EasyRdf_Graph();
-$graph->parseFile("../data/foaf.rdf"); // https://groups.google.com/forum/#!topic/easyrdf/flX3d2e4U3Q
-$me = $graph->primaryTopic();
+$graph->parseFile("../data/foaf.rdf", "rdfxml"); // https://groups.google.com/forum/#!topic/easyrdf/flX3d2e4U3Q
+$me = $graph->primaryTopic("http://njh.me/foaf.rdf");
 echo "My name is: ".$me->get('foaf:name')."\n";
 ?>
